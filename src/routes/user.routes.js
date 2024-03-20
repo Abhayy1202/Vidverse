86471184b3a182e312e2b026e7b598eaaa154848
Abhayy1202 <barry59634@gmail.com>
2024-03-21 02:56:52 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refresh_accessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js "
@@ -29,5 +36,17 @@ router.route("/login").post(loginUser);
 //secure route
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refresh_accessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar-Update").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-Update").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/history".get(verifyJWT,getWatchHistory))
+
+//req.params se utha rahe hai data so ye colon ke baad wala item hi params me rhta hai 
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
+
+
+
 
 export default router;
